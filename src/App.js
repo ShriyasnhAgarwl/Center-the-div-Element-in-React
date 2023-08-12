26dd86291646556ba2ebb3cbbfaa6ab9bd515981
SHRIYANSH AGARWAL <shriyanshagar9026@gmail.com>
2023-08-12 07:56:13 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="Main-page">
+      <div className="App">
+        <h1>Hello Shriyansh!</h1>
+        <h2>Lets do make it happen, and crack the code!</h2>
+      </div>
+    </div>
+  );
+}
